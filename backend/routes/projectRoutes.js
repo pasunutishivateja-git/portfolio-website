e170c88b9e8c541213e1bf6de9974d206963aa23
Shiva Teja <pasunutishivateja@gmail.com>
2026-05-20 22:31:09 +0530

    fixed lowercase project import

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Project = require("../models/Project");
-
+const Project = require("../models/project");
 
 // CREATE PROJECT
 router.post("/", async (req, res) => {
