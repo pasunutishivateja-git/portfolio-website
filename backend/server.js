77e09dae6c805a5c4658e1e76f4c58c6e0149a7b
Shiva Teja <pasunutishivateja@gmail.com>
2026-05-20 23:37:39 +0530

    debug mongo uri

--- backend/server.js
+++ backend/server.js
@@ -25,5 +25,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("MONGO_URI =", process.env.MONGO_URI);
 });
